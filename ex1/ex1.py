09196967bd0abe70a107431d9020e244ac110e08
AlexCorpROS <arcorprintr@gmail.com>
2024-02-07 15:06:51 +0300

    task

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,7 +1,56 @@
-def main():
-    pass
+import  argparse
+from collections import namedtuple
+import  os
+import logging
+import json
+
+FORMAT = ("{asctime} - {levelname}: {msg}")
+
+logging.basicConfig(filename='log/ex15_1.log', filemode='a', format=FORMAT, encoding='utf-8', style='{', level=logging.INFO)
+logger = logging.getLogger('main')
+
+parser = argparse.ArgumentParser(description="argument parser")
+parser.add_argument('-p', metavar='path', type= str or None , nargs='*', default=None, help='Укажите путь к директории или файлу')
+args = parser.parse_args()
 
 
+def split_path(lst_path: str):
+    fs_objects = []
+    if lst_path is None:
+        path_string = os.getcwd()
+        logger.warning(f'Пустой ввод.Путь установлен по умолчанию {path_string}')
+        abs_path, full_name = os.path.split(path_string)
+        abs_parent, parent = os.path.split(abs_path)
+        name = full_name
+        expansion = False
+        flag = True
+    else:
+        path_string = lst_path[0]
+        '''
+        Родительский каталог определяется на один уровень вверх.
+        При необходимости укзать абсолютный путь до файла/каталога вместо parent берем abs_path
+        '''
+        abs_path, full_name = os.path.split(path_string)
+        abs_parent, parent = os.path.split(abs_path)
+        if '.' in full_name:
+            name, expansion = full_name.split('.')
+            flag = False
+            logger.info(f'Выбран файл {full_name}.Абсолютный путь к файлу {abs_path}')
+        else:
+            name = full_name
+            expansion = False
+            flag = True
+            logger.info(f'Выбран каталог {full_name}. Абсолютный путь к каталогу {abs_path}.')
+
+    Obj_info = namedtuple('Obj_info', 'name expansion catalog home', defaults=['', '', False, ''])
+    fs_objects.append(Obj_info(name=name, expansion=expansion, catalog=flag, home=parent))
+    logger.info(f'Записаны данные {fs_objects}')  # Запись данных в логи
+    with open('base.json', 'a') as file:  # Запись данных в файл
+        json.dump(fs_objects, file)
+        file.write('\n')
+    return fs_objects
 
 if __name__ == '__main__':
-    main()
+    print(split_path(args.p))
+
+# pyton .\ex15_1.py -p C:\Work\PyCharm\PytoProject\venv\hw.exe
--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,5 +1,39 @@
-def main():
-    pass
+import logging
+import  argparse
+
+FORMAT = ("{asctime} - {levelname}: {msg}")
+logging.basicConfig(filename='log/ex15_2.log', filemode='a', format=FORMAT, encoding='utf-8', style='{', level=logging.INFO)
+logger = logging.getLogger('main')
+
+parser = argparse.ArgumentParser(description="argument parser")
+parser.add_argument('-a', metavar=' side_triangle_a', type= int or float , default=1, help='Задайте сторону А')
+parser.add_argument('-b', metavar=' side_triangle_b', type= int or float , default=1, help='Задайте сторону B')
+parser.add_argument('-c', metavar=' side_triangle_c', type= int or float , default=1, help='Задайте сторону C')
+args = parser.parse_args()
+
+def triangle(a = 1, b = 1, c=1):
+    if a + b > c and a + c > b and b + c > a:
+        print("Треугольник существует")
+        if a == b == c:
+            print("Треугольник равносторонний")
+            logger.info(f'Треугольник равносторонний. Стороны а = {a}, b = {b}, c = {c}')
+        elif a == b or a == c or b == c:
+            print("Треугольник равнобедренный")
+            logger.info(f'Треугольник равнобедренный. Стороны а = {a}, b = {b}, c = {c}')
+        else:
+            print("Треугольник разносторонний")
+            logger.info(f'Треугольник разносторонний. Стороны а = {a}, b = {b}, c = {c}')
+    else:
+        print("Треугольник не существует")
+        logger.warning(f'Треугольник со сторонами а = {a}, b = {b}, c = {c} не существует.')
+
 
 if __name__ == '__main__':
-    main()
+    triangle(float(args.a),float(args.b),float(args.c))
+
+# if __name__ == '__main__':
+#     triangle(2,1,1)
+#     print('-----------')
+#     triangle(222,2,2)
+
+# pyton .\ex15_2.py -a 2 -b 2 -b 2
